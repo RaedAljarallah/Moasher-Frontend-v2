69d0e55f457b736cdbe8e37bf4e960706b17fa74
Raed <aljarallah.raed@gmail.com>
2022-10-29 19:00:10 +0300

    WIP On User pages

---
+++ src/app/pages/users/users.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+
+@NgModule({
+  declarations: [
+    UsersListComponent,
+    UserFormComponent
+  ],
+  imports: [
+    CommonModule,
+    UsersRoutingModule
+  ],
+  exports: [
+    UsersListComponent,
+    UserFormComponent
+  ]
+})
+export class UsersModule { }
